48ab5b67b22bf3a43d1674cf69fb49e27f99932f
rnparks <rnparks@gmail.com>
2016-04-15 13:42:43 -0400

    yield page and test fixed

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def home
-    @response = HTTParty.get('https://www.sec.gov/Archives/edgar/data/728535/000143774916025879/jbht20151231_10k.htm')
   end
 
   def help
